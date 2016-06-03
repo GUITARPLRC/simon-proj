e926eb50f56ae6c4cd38d31429f9f2c85c1e37d1
Charles <GUITARPLRC@YAHOO.COM>
2016-06-03 13:23:45 -0500

    fixed check for score count and correct selection
    need fix re-loop sequence and light up

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
 var red = document.getElementById("red"),
-		green = document.getElementById("green"),
-		blue = document.getElementById("blue"),
-		yellow = document.getElementById("yellow"),
-		start = document.getElementById("start"),
-		end = document.getElementById("end"),
-		randNum = 0,
-		score = 0,
-		sequence = [],
-		check = [],
+    green = document.getElementById("green"),
+    blue = document.getElementById("blue"),
+    yellow = document.getElementById("yellow"),
+    start = document.getElementById("start"),
+    end = document.getElementById("end"),
+    randNum = 0,
+    score = 0,
+    sequence = [2,3],
+    check = [],
     i = 0;
-
+    
 start.addEventListener("click", chooseColor, false);
 end.addEventListener("click", endGame, false);
 
-red.addEventListener("click", checkSequence(1), false);
-green.addEventListener("click", checkSequence(2), false);
-blue.addEventListener("click", checkSequence(3), false);
-yellow.addEventListener("click", checkSequence(4), false);
+red.addEventListener("click", function() { checkSequence(1) }, false);
+green.addEventListener("click", function() { checkSequence(2) }, false);
+blue.addEventListener("click", function() { checkSequence(3) }, false);
+yellow.addEventListener("click", function() { checkSequence(4) }, false);
 
 red.disabled = true;
 green.disabled = true;
@@ -26,19 +26,19 @@ yellow.disabled = true;
 end.disabled = true;
 
 function randomNum() {
-	
-	randNum = Math.floor((Math.random() * 4) + 1);
-	
+  
+  randNum = Math.floor((Math.random() * 4) + 1);
+  
 }
 
 function chooseColor() {
-	
-	randomNum(); //GENERATE RANDOM NUMBER
-	sequence.push(randNum); //PUSH RANDOM NUMBER TO ARRAY
-		
-	(function loop(){
-		
-			
+  
+  randomNum(); //GENERATE RANDOM NUMBER
+  sequence.push(randNum); //PUSH RANDOM NUMBER TO ARRAY
+    
+  (function loop(){
+    
+      
     switch(sequence[i]) { //ILLUMINATE COLORS IN SEQUENCE
     
       case 1: //1 = red
@@ -72,38 +72,38 @@ function chooseColor() {
       setTimeout(loop, 1000);
     }
     
-	})(); // end loop function
+  })(); // end loop function
   
-	start.disabled = true;
-	end.disabled = false;
+  start.disabled = true;
+  end.disabled = false;
   
-	check = sequence; // SET TO NOT MESS WITH ORGINAL ORDER
+  check = sequence; // SET TO NOT MESS WITH ORGINAL ORDER
   
   red.disabled = false;
   green.disabled = false;
   blue.disabled = false;
   yellow.disabled = false;
-	
+  
 }
 
 function showColor(a){ //MAKE COLORS APPEAR "ON"
 
-	a.className += " select";
-	
+  a.className += " select";
+  
 }
 
 function removeColor(b) { //MAKE COLORS APPEAR "OFF"
-	
-	b.classList.remove("select");
-	
+  
+  b.classList.remove("select");
+  
 }
 
 function checkSequence(n) { //CHECK IF INPUT IS IN CORRECT ORDER
-	
-	if (n === check[0]) { //INCREASE SCORE AND REMOVE FIRST INDEX
-		
-		score++;
-		check.shift();
+  score++;
+
+  if (n === check[0]) { //INCREASE SCORE AND REMOVE FIRST INDEX
+  
+    check.splice(0,1);
     
     if (check.length === 0) {
     
@@ -114,22 +114,22 @@ function checkSequence(n) { //CHECK IF INPUT IS IN CORRECT ORDER
       yellow.disabled = true;
     
     }
-		
-	} else { //WRONG CHOICE = GAME OVER!
-		
-		endGame();
-		
-	}
-	
+    
+  } else { //WRONG CHOICE = GAME OVER!
+    
+    endGame();
+    
+  }
+ 
 }
 
 function endGame() {
-	
+  
   i = 0;
   sequence = [];
-	check = [];
-	score = 0;
-	start.disabled = false;
-	end.disabled = true;
-	
+  check = [];
+  score = 0;
+  start.disabled = false;
+  end.disabled = true;
+  
 }
